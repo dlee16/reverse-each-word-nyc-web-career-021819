2f5d8ec6764eee1b0a6dd95cb8951a96c09395e0
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 19:55:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence1)
   new = sentence1.split(" ")
   reversed =[]
-  new.each do |char| reversed.unshift(char)}
+  new.each do |char| 
+  reversed.unshift(char)
 return reversed.join(" ")
 end
