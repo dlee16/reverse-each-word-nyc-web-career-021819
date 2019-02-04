250a3fd4181e2e18ba6d6c0b173df636ed9f66b5
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 19:44:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   new = sentence1.split(" ")
   reversed =[]
-  new.each {|words| puts words.reverse}
+  sentence1.each {|words| puts words.reverse}
   reversed.join
 end
 end
